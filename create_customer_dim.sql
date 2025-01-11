d76bf70b7f7f611ca4f137d905772189131a5404
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-01-11 14:07:27 +0200

    adding customer dimension table

---
+++ create_customer_dim.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CUSTOMER_DIMENSION (
+    CustomerID      NUMBER(10)        NOT NULL,         -- Unique identifier for the customer
+    FirstName       VARCHAR2(50)      NOT NULL,         -- First name of the customer
+    LastName        VARCHAR2(50)      NOT NULL,         -- Last name of the customer
+    Email           VARCHAR2(100),                      -- Email address of the customer
+    Phone           VARCHAR2(15),                       -- Phone number of the customer
+    Address         VARCHAR2(255),                      -- Address of the customer
+    MembershipDate  DATE              NOT NULL,         -- Date of membership registration
+    CONSTRAINT PK_CUSTOMER_DIM PRIMARY KEY (CustomerID) -- Primary key constraint
+);
