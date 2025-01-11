3296924f7281a080d832b8c118db7f374fa393b5
Mohammed Makbool <M.Makbool@outlook.sa>
2025-01-11 17:48:09 +0200

    category dim file edited

--- create_category_dim.sql
+++ create_category_dim.sql
@@ -1,4 +1,4 @@
-CREATE TABLE PRODUCT_DIM (
+CREATE TABLE CATEGORY_DIM (
     CATEGORYID      NUMBER(10)            ,                -- Unique identifier for the CATEGORY
     CATEGORYNAME    VARCHAR2(50)  NOT NULL,                -- Name of the CATEGORY
     DESCRIPTION     CLOB                  ,                -- Description of the CATEGORY
